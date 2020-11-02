7d8792e5246fb66cfb9d2ff1f81283f8d3daeaff
Minsi Yang <yangminsi@gmail.com>
2020-11-02 17:28:22 +0000

    Add test for Birthday class #birthday, test passed

--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Birthday do
     bday = Birthday.new("Annie Hall", "19/08/2020")
     expect(bday.name).to eq("Annie Hall")
   end
+
+  it "knows the date of the birthday" do
+    bday = Birthday.new("Annie Hall", "19/08/2020")
+    expect(bday.birthday).to eq("19/08/2020")
+  end
 end
