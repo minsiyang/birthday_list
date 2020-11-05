0617f99c3df96e1a254479b0e4523b2cdd997706
Minsi Yang <yangminsi@gmail.com>
2020-11-05 11:53:14 +0000

    Refactored Birthday spec

--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -2,29 +2,27 @@ require 'birthday'
 require 'timecop'
 
 RSpec.describe Birthday do
+  let(:bday) { Birthday.new }
+
   it 'knows it is date' do
-    bday = Birthday.new
     result = bday.birthday("19/08/2020")
     actual = Time.parse("19/08/2020")
     expect(result).to eq(actual)
   end
 
   it 'will return false if today is not the birthday' do
-    bday = Birthday.new
     bday.birthday("19/08/2020")
     expect(bday.today?("19/08/2020")).to eq false
   end
 
   it 'will return true if today is the birthday' do
     Timecop.freeze("19/08/2020") do
-      bday = Birthday.new
       bday.birthday("19/08/2020")
       expect(bday.today?("19/08/2020")).to eq true
     end
   end
 
   it 'can return the age' do
-    bday = Birthday.new
     bday.birthday("19/08/2019")
     result = bday.age("19/08/2019")
     expect(result).to eq 1
