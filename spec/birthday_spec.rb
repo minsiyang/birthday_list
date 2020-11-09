1d516eb58216c4454e1c73c77d3e10b6876deac5
Minsi Yang <yangminsi@gmail.com>
2020-11-09 11:54:18 +0000

    Still don't know how to mock instance_double for #view-birthday

--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -1,5 +1,5 @@
 require 'birthday'
-# require 'timecop'
+require 'timecop'
 
 RSpec.describe Birthday do
   it "knows the name for the birthday" do
@@ -16,18 +16,6 @@ RSpec.describe Birthday do
     bday = Birthday.new("Annie Hall", "19/08/2019")
     expect(bday.age).to eq(1)
   end
-
-  # it 'will return false if today is not the birthday' do
-  #   bday.birthday("19/08/2020")
-  #   expect(bday.today?("19/08/2020")).to eq false
-  # end
-
-  # it 'will return true if today is the birthday' do
-  #   Timecop.freeze("19/08/2020") do
-  #     bday.birthday("19/08/2020")
-  #     expect(bday.today?("19/08/2020")).to eq true
-  #   end
-  # end
 
   # it 'can return the age' do
   #   bday.birthday("19/08/2019")
